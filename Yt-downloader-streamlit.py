e835229dc9dcd48e3088132e06791f366ba57409
STAR SIWACH <76273539+Ashwani132003@users.noreply.github.com>
2022-07-28 20:10:05 +0530

    Update Yt-downloader-streamlit.py

--- Yt-downloader-streamlit.py
+++ Yt-downloader-streamlit.py
@@ -2,8 +2,8 @@
 from pytube import YouTube
 
 from streamlit import *
-from pathlib import Path
-downloads_path = str(Path.home() / "Downloads")
+# from pathlib import Path
+# downloads_path = str(Path.home() / "Downloads")
 
 import streamlit 
 
@@ -31,7 +31,13 @@ if link!='':
     else:
         video = video.streams.get_highest_resolution()
         if button(label='Download'):
-            video.download(downloads_path)
+            try:
+                folder = subprocess.run(["xdg-user-dir", "DOWNLOAD"],
+                            capture_output=True, text=True).stdout.strip("\n")
+            except FileNotFoundError:  # if the command is missing
+                import os.path
+                folder = os.path.expanduser("~/Downloads") 
+            video.download(folder)
             success('Download Started Please check video in your device folder..')
             warning('Thanks for using this website 😊') 
     
